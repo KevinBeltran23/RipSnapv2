fb30ead9c00a62559cc8eb7781431df1b7a81804
KevinBeltran23 <kevinbeltran2437@gmail.com>
2026-02-24 18:06:10 -0800

    env

---
+++ app.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@expo/config').ConfigContext} */
+module.exports = ({ config }) => {
+    return {
+        ...config,
+        ios: {
+            ...config.ios,
+            googleServicesFile:
+                process.env.GOOGLE_SERVICES_INFO_PLIST ?? './GoogleService-Info.plist',
+        },
+        android: {
+            ...config.android,
+            googleServicesFile:
+                process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+        },
+    };
+};
