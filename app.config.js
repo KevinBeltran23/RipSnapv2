0a1b20fd185fad4824e2c6b55a78abebf25bd930
KevinBeltran23 <kevinbeltran2437@gmail.com>
2026-02-24 22:42:30 -0800

    env

--- app.config.js
+++ app.config.js
@@ -6,11 +6,19 @@ module.exports = ({ config }) => {
             ...config.ios,
             googleServicesFile:
                 process.env.GOOGLE_SERVICES_INFO_PLIST ?? './GoogleService-Info.plist',
+            config: {
+                googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
+            },
         },
         android: {
             ...config.android,
             googleServicesFile:
                 process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+            config: {
+                googleMaps: {
+                    apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
+                },
+            },
         },
     };
 };
